c3f9925f140c4665cd960fce36b19a700696fa19
Colin Dunklau <colin.dunklau@gmail.com>
2019-12-31 16:54:34 +0100

    Add setup.py and tox

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='gistit',
+    version='0.1.0-dev',
+    author='Colin Dunklau',
+    author_email='colin.dunklau@gmail.com',
+    py_modules=['gistit'],
+    install_requires=['requests >=2.0,<3.0'],
+)
